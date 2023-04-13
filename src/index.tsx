dff56dbded832dc1f943c86ec7a4c3f7040d1d9e
sbuggay <sbuggay@gmail.com>
2023-04-12 23:42:54 -0700

    Refactor story and comment hydration

--- src/index.tsx
+++ src/index.tsx
@@ -1,72 +1,29 @@
 import * as ReactDOMServer from 'react-dom/server';
-import { getItem, getTopStories } from './util';
+import { IHydrationStats, getTopStories, hydrateComments } from './util';
 import * as fs from 'fs';
-import { IComment, IItem } from './Items';
 import * as path from 'path';
 import StoryPreview from './components/StoryPreview';
 import Story from './components/Story';
-import Page from './components/Page';
 import About from './components/About';
+import { StaticPage } from './StaticPage';
 
 const LIMIT = 30;
 const outputDirectory = './build';
 const resourceDirectory = './src/resources';
 
-export interface IHyrdationStats {
-    networkCount: number;
-    maxDepth: number;
-}
-
-async function hydrateComments(parent: IItem, depth: number, stats: IHyrdationStats, root: IComment | undefined = undefined): Promise<number> {
-    const { kids } = parent;
-
-    if (!parent || !kids) {
-        parent.more = 1;
-        return parent.more;
-    }
-
-    let children = kids;
-    children = children.slice(0, depth === 0 ? 30 : 20);
-
-    // Grab the comments
-    const comments = await Promise.all(children.map(id => getItem(id) as Promise<IComment>));
-
-    stats.networkCount += comments.length;
-    stats.maxDepth = Math.max(stats.maxDepth, depth);
-
-    // Hydrate their children
-    const more = await Promise.all(comments.map(async (comment) => {
-        comment.depth = depth;
-
-        if (!root) {
-            root = comment;
-        }
-
-        comment.root = root;
-        return await hydrateComments(comment, depth + 1, stats, root);
-    }));
-    parent.comments = comments;
-    parent.more = more.reduce((acc, v) => acc + v, 0) + 1;
-
-    return parent.more;
-}
-
 async function generate() {
     const start = performance.now();
 
-    const stories = await getTopStories(LIMIT);
-
-    const index =
-        <Page>
-            <ul>
-                {stories.map(story => <StoryPreview story={story} key={story.title} />)}
-            </ul>
-        </Page>;
-
+    fs.rmSync(outputDirectory, { recursive: true, force: true });
     fs.mkdirSync(outputDirectory, { recursive: true });
 
-    const output = ReactDOMServer.renderToStaticMarkup(index);
-    fs.writeFileSync(path.join(outputDirectory, 'index.html'), output);
+    const stories = await getTopStories(LIMIT);
+
+    const index = new StaticPage(<ul>
+        {stories.map(story => <StoryPreview story={story} key={story.title} />)}
+    </ul>, path.join(outputDirectory, 'index.html'));
+
+    index.write();
 
     console.log(`id\t\tvolume\t\tmax depth`);
 
@@ -76,7 +33,7 @@ async function generate() {
     // Build a story page for each story
     await Promise.all(stories.map(async (story) => {
 
-        const stats: IHyrdationStats = {
+        const stats: IHydrationStats = {
             networkCount: 0,
             maxDepth: 0
         }
@@ -84,15 +41,12 @@ async function generate() {
         const count = await hydrateComments(story, 0, stats);
         story.descendants = count; // Filter out unused descendants and use our traversed one
 
-        const root = <Page><Story story={story} /></Page>;
-
         console.log(`${story.id}\t\t${stats.networkCount}\t\t${stats.maxDepth}`);
-
         totalNetworkRequests += stats.networkCount;
         maxDepth = Math.max(maxDepth, stats.maxDepth);
 
-        const storyOutput = ReactDOMServer.renderToStaticMarkup(root);
-        fs.writeFileSync(path.join(outputDirectory, `${story.id}.html`), storyOutput);
+        const storyPage = new StaticPage(<Story story={story} />, path.join(outputDirectory, `${story.id}.html`));
+        storyPage.write();
     }));
 
     // Copy static resources
@@ -101,14 +55,13 @@ async function generate() {
     const duration = performance.now() - start;
     console.log('Total duration: ' + duration + 'ms');
 
-    // Generate about page
-    const aboutPageOutput = ReactDOMServer.renderToStaticMarkup(<About stats={{
+    const aboutPage = new StaticPage(<About stats={{
         timestamp: new Date(),
         totalNetworkRequests,
         maxDepth,
         duration: Math.round(duration)
-    }} />);
-    fs.writeFileSync(path.join(outputDirectory, `about.html`), aboutPageOutput);
+    }} />, path.join(outputDirectory, `about.html`));
+    aboutPage.write();
 }
 
 generate();
--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,7 @@
 import * as https from "https";
 import * as fs from "fs";
 import * as path from "path";
-import { IItem, IStory } from "./Items";
+import { IComment, IItem, IStory } from "./Items";
 
 export function fetch(url: string) {
     return new Promise<string>((resolve, reject) => {
@@ -37,6 +37,57 @@ export async function getItem<T extends IItem>(id: number): Promise<T> {
             return JSON.parse(res) as T;
         }
     );
+}
+
+export interface IHydrationStats {
+    networkCount: number;
+    maxDepth: number;
+}
+
+export async function hydrateComments(parent: IItem, depth: number, stats: IHydrationStats, root: IComment | undefined = undefined): Promise<number> {
+    const { kids } = parent;
+
+    if (!parent || !kids) {
+        parent.more = 1;
+        return parent.more;
+    }
+
+    let children = kids;
+    children = children.slice(0, depth === 0 ? 30 : 20);
+
+    // Grab the comments
+    const comments = await Promise.all(children.map(id => getItem(id) as Promise<IComment>));
+
+    stats.networkCount += comments.length;
+    stats.maxDepth = Math.max(stats.maxDepth, depth);
+
+    for (let i = 0; i < comments.length - 1; i++) {
+        const comment = comments[i];
+        if (i > 0) {
+            comment.prev = comments[i - 1];
+        }
+        else if (depth > 0) {
+            comment.prev = parent as IComment;
+        }
+        if (i < (comments.length - 1)) {
+            comment.next = comments[i + 1];
+        }
+        else if (depth > 0) {
+            comment.next = (parent as IComment).next;
+        }
+    }
+
+    // Hydrate their children
+    const more = await Promise.all(comments.map(async (comment) => {
+        comment.depth = depth;
+        comment.root = root;
+        return await hydrateComments(comment, depth + 1, stats, root);
+    }));
+
+    parent.comments = comments;
+    parent.more = more.reduce((acc, v) => acc + v, 0) + 1;
+
+    return parent.more;
 }
 
 export function clearDirSync(dirPath: string) {
